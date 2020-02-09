cb201afa80022a5f2290266e1ea5f47640590035
Choreas <niklas.kosanke@web.de>
2020-02-09 23:59:20 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HexagonalGrid.hexagonalGrid("110", "110"));
+        }
+    }
+}
