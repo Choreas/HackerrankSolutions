72cbcd999a7c85b4131c3b5518e3ee5167e36c4d
Choreas <niklas.kosanke@web.de>
2020-02-10 20:03:43 +0100

    Kangaroo added.

--- ConsoleTester/LibTester.cs
+++ ConsoleTester/LibTester.cs
@@ -8,7 +8,7 @@ namespace ConsoleTester
     {      
         static void Main(string[] args)
         {
-            appleAndOrange();
+            kangaroo();
         }
 
         static void hexagonalGrid()
@@ -36,6 +36,20 @@ namespace ConsoleTester
             int[] oranges = new int[] { 5, -6 };            
 
             AppleAndOrange.countApplesAndOranges(s, t, a, b, apples, oranges);
+        }
+
+        static void kangaroo()
+        {            
+            string[] x1V1X2V2 = new string[] {"0","3","4","2" };
+
+            int x1 = Convert.ToInt32(x1V1X2V2[0]);
+            int v1 = Convert.ToInt32(x1V1X2V2[1]);
+            int x2 = Convert.ToInt32(x1V1X2V2[2]);
+            int v2 = Convert.ToInt32(x1V1X2V2[3]);
+
+            string result = Kangaroo.kangaroo(x1, v1, x2, v2);
+
+            Console.WriteLine(result);
         }
     }
 }
