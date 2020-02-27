30a1114f490010ec133e22b9df2c91378dcb4bab
Choreas <niklas.kosanke@web.de>
2020-02-27 20:24:57 +0100

    Prepared project for solutions in Go.

---
+++ Go/HackerrankSolutions/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/Choreas/HackerrankSolutions/HackerrankSolutions/Algorithms/Implementation/implementation"
+	"fmt"
+)
+
+func main() {
+	var i := implementation.DrawingBook()
+	fmt.Println(i)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Choreas/HackerrankSolutions
+
+go 1.13
