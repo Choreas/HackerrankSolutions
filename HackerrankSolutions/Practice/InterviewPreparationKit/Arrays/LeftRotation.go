829b4ed2fb21323885e860690ccc871acc09fcb9
Choreas <niklas.kosanke@web.de>
2020-03-10 22:53:18 +0100

    Revised Left Rotation.

--- HackerrankSolutions/Practice/InterviewPreparationKit/Arrays/LeftRotation.go
+++ HackerrankSolutions/Practice/InterviewPreparationKit/Arrays/LeftRotation.go
@@ -2,22 +2,22 @@ package arrays
 
 //RotLeft rotates an array a by d iterations.
 func RotLeft(a []int32, d int32) []int32 {
-	var last int32 = 0
 	var length = len(a)
 	var length32 = int32(length)
+	var b []int32
 	if d >= length32 {
 		d = d % length32
 	}
-	for rotation := int32(1); rotation <= d; rotation++ {
-		last = a[0]
-		for idx := range a {
-			if idx == length-1 {
-				a[idx] = last
-			} else {
-				a[idx] = a[idx+1]
-			}
+
+	for idx := range a {
+		var newidx = d + int32(idx)
+		if newidx >= length32 {
+			newidx -= length32
 		}
+
+		b = append(b, a[newidx])
+
 	}
 
-	return a
+	return b
 }
